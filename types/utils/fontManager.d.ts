1606cc8f7ca6330163b1e695e474e30428d1bf30
edelvarden <edelvarden@gmail.com>
2024-06-29 13:23:33 +0300

    bump version

--- types/utils/fontManager.d.ts
+++ types/utils/fontManager.d.ts
@@ -1,20 +1,23 @@
 /**
- * Disable style tag and remove inline styles
+ * Disables the style tag and removes inline styles
  */
 export declare const cleanupStyles: () => void
 /**
- * Toggle style tag enabled state
+ * Toggles the enabled state of the style tag
  */
 export declare const toggleStyleTag: (styleId: string, enable: boolean) => void
 /**
- * Create style tag or update styles content when different
+ * Creates or updates the style tag's content when different
  */
 export declare const createOrUpdateStyleTag: (id: string, content: string) => void
 /**
- * Reinitialize settings to start preview by updating injected styles
+ * Reinitializes settings to start preview by updating injected styles
  */
 export declare const preview: () => Promise<void>
 /**
- * Initialize main functions
+ * Initializes main functions
  */
-export declare const init: (settings: { 'font-default': string; 'font-mono': string }) => void
+export declare const init: (settings: {
+  'font-default': string
+  'font-mono': string
+}) => Promise<void>
